c1e9454153d51c48f530c21fa818c6103fee3148
Matheus Moreira De Souza <matheussouza@brq.com>
2018-07-06 10:48:47 -0300

    TempoTimeouts removido

--- src/br/com/brq/teste/TempoTimeouts.java
+++
@@ -1,6 +0,0 @@
-package br.com.brq.teste;
-
-public abstract class TempoTimeouts {
-	public final static int TEMPOLONGO = 20000, TEMPOMEDIO = 4000, TEMPOCURTO = 2000, TEMPOPADRAOTELA = 1000,
-			TEMPOWAIT = 50, TEMPOPOLLING = 2;
-}
